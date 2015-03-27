2b97e15cb5be3a5eb41d23c571721f8b404a7e94
Alexandre Macedo <alexbmac@gmail.com>
2015-03-27 09:59:39 -0300

    ..

--- problems/problem009.hs
+++ problems/problem009.hs
@@ -11,6 +11,32 @@
 
 module Main where
 
-main :: IO ()
+nextTriplet :: (Ord a, Num a) => a -> (a, a, a) -> (a, a, a)
+nextTriplet totalSum (a,b,c)
+    | b + 1 < c - 1  = (a, b+1, c-1)
+    | a + 1 >= b     = error "No more triplets available!"
+    | otherwise      = (a+1, a+2, totalSum - 2 * a - 3)
 
+
+nextTripletOf1000 :: (Ord a, Num a) => (a, a, a) -> (a, a, a)
+nextTripletOf1000 = nextTriplet 1000
+
+
+isPythagorean :: (Num a) => (a, a, a) -> Bool
+isPythagorean (a,b,c) = a^2 + b^2 = c^2
+
+
+tripleProduct (Num a) => (a, a, a) -> a
+tripleProduct (a, b, c) = a * b * c
+
+
+-- findTriplet :: (Num a) => (b -> Bool) -> (a, a, a)
+findTriplet pred triplet getNext
+    | pred triplet = triplet
+    | otherwise    = findtriplet pred (getNext triplet) getNext
+
+
+main = do
+    let triplet = findTriplet isPythagorean (1,2,997) nextTripletOf1000
+    in print $ tripleProduct triplet
 
