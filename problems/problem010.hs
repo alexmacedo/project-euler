a5cece9dd936ea57eedb265b29d293099e9205fc
Alexandre Macedo <alexbmac@gmail.com>
2015-03-28 15:02:52 -0300

    problem 10 and 13 solved

--- problems/problem010.hs
+++ problems/problem010.hs
@@ -10,6 +10,22 @@
 
 module Main where
 
-main :: IO ()
 
+isPrime :: (Integral a) => a -> [a] -> Bool
+isPrime _ [] = True
+isPrime n (x:xs)
+    | 2*x > n        = True
+    | n `mod` x /= 0 = isPrime n xs
+    | otherwise      = False
+
+
+allPrimes :: (Integral a) => a -> [a] -> [a]
+allPrimes _ [] = []
+allPrimes limit xs
+    | (last xs) > limit  = init xs
+    | isPrime (last xs) (init xs) = allPrimes limit (xs ++ [(last xs) + 2])
+    | otherwise    = allPrimes limit ((init xs) ++ [(last xs) + 2])
+
+main = do
+    print $ sum $ allPrimes 2000000 [2,3,5,7,11,13]
 
